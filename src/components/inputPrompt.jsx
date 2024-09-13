a31672dd5d51dc00273f7c6da5977fccfe259990
Karlo Cehulic <karlocehlic@gmail.com>
2024-09-13 04:28:57 +0200

    Create div for input

---
+++ src/components/inputPrompt.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "../styles/inputs.css"
+
+export function CreationInput() {
+    return (
+        <div className="creation-input">
+            <h1>Test Content</h1>
+        </div>
+    )
+}
